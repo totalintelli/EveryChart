1fc01152f14cc6ccf29473d7b413fb7beabdf634
totalintelli <blueintelli@gmail.com>
2016-05-27 17:20:45 +0900

    프로젝트 생성. 제목과 만 권과 60과 50을 그림

---
+++ FrmEveryChart.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EveryChart
+{
+    public partial class FrmEveryChart : Form
+    {
+        public FrmEveryChart()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBarGraph_Click(object sender, EventArgs e)
+        {
+            // 제목 글자열
+            string Title = "월별 책의 판매량";
+            // 제목의 폰트
+            Font TitleFont = new Font("SansSerif", 18);
+            // 제목의 색상
+            SolidBrush TitleBrush = new SolidBrush(Color.Blue);
+            // 제목의 위치
+            PointF TitlePoint = new PointF(Width / 2.0f, 0); // 1/ 2.0f는 제목의 위치를 정하기 위한 값으로 고정값.
+            // Y축 글자열
+            // (만 권)
+            string KwanText = "(만 권)";
+            // (만 권)의 색상
+            SolidBrush TextBrush = new SolidBrush(Color.Teal);
+            // (만 권)의 위치
+            PointF KwanTextPoint = new PointF(Width / 8.0f, Height / 11.0f);
+            // 60
+            string SixtyText = "60";
+            // 60의 위치
+            PointF SixtyTextPoint = new PointF(Width * 3 / 16.0f, Height * 2 / 11.0f);
+            // 50
+            string FiftyText = "50";
+            // 50의 위치
+            PointF FiftyTextPoint = new PointF(Width * 3 / 16.0f, Height * 3 / 11.0f);
+
+            Graphics g = panel1.CreateGraphics();
+            // 제목을 그린다.
+            g.DrawString(Title, TitleFont, TitleBrush, TitlePoint);
+
+            // (만 권)을 그린다.
+            g.DrawString(KwanText, TitleFont, TextBrush, KwanTextPoint);
+
+            // 60을 그린다.
+            g.DrawString(SixtyText, TitleFont, TextBrush, SixtyTextPoint);
+
+            // 50을 그린다.
+            g.DrawString(FiftyText, TitleFont, TextBrush, FiftyTextPoint);
+        }
+    }
+}
