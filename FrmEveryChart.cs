62a143c376f4a5b28679cfe9fccc6fcfe0d7061a
totalintelli <kwanwoo5@naver.com>
2016-05-29 22:12:15 +0900

    주석 수정.

--- FrmEveryChart.cs
+++ FrmEveryChart.cs
@@ -19,6 +19,8 @@ namespace EveryChart
 
         private void btnLineGraph_Click(object sender, EventArgs e)
         {
+            // panel1에 그릴 준비를 한다.
+            Graphics g = panel1.CreateGraphics();
             // 제목 글자열
             string Title = "월별 책의 판매량";
             // 제목의 폰트
@@ -34,39 +36,33 @@ namespace EveryChart
             SolidBrush TextBrush = new SolidBrush(Color.Teal);
             // (만 권)의 위치
             PointF KwanTextPoint = new PointF(Width / 8.0f, Height / 11.0f);
-            // 60
+            // 세로축에 쓰는 60
             string SixtyText = "60";
-            // 60의 위치
+            // 세로축에 쓰는 60의 위치
             PointF SixtyTextPoint = new PointF(Width * 3 / 16.0f, Height * 2 / 11.0f);
-            // 50
+            // 세로축에 쓰는 50
             string FiftyText = "50";
-            // 50의 위치
+            // 세로축에 쓰는 50의 위치
             PointF FiftyTextPoint = new PointF(Width * 3 / 16.0f, Height * 3 / 11.0f);
-
-            Graphics g = panel1.CreateGraphics();
-            // 40
+            // 세로축에 쓰는 40
             string FourtyText = "40";
-            // 40의 위치
+            // 세로축에 쓰는 40의 위치
             PointF FourtyTextPoint = new PointF(Width * 3 / 16.0f, Height * 4 / 11.0f);
-
-            // 30
+            // 세로축에 쓰는 30
             string ThirtyText = "30";
-            // 30의 위치
+            // 세로축에 쓰는 30의 위치
             PointF ThirtyTextPoint = new PointF(Width * 3 / 16.0f, Height * 5 / 11.0f);
-
-            // 20
+            // 세로축에 쓰는 20
             string TwentyText = "20";
-            // 20의 위치
+            // 세로축에 쓰는 20의 위치
             PointF TwentyTextPoint = new PointF(Width * 3 / 16.0f, Height * 6 / 11.0f);
-
-            // 10
+            // 세로축에 쓰는 10
             string TenText = "10";
-            // 10의 위치
+            // 세로축에 쓰는 10의 위치
             PointF TenTextPoint = new PointF(Width * 3 / 16.0f, Height * 7 / 11.0f);
-
-            // 0
+            // 세로축에 쓰는 0
             string ZeroText = "0";
-            // 0의 위치
+            // 세로축에 쓰는 0의 위치
             PointF ZeroTextPoint = new PointF(Width * 3 / 16.0f, Height * 7.5f / 11.0f);
 
             // 제목을 그린다.
