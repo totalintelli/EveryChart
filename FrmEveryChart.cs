46610bd5e8fc046904660bb3e242f094d6282022
totalintelli <blueintelli@gmail.com>
2016-05-31 16:02:33 +0900

    차트에 있는 숫자를 그리는 알고리즘을 리팩토링함.

--- FrmEveryChart.cs
+++ FrmEveryChart.cs
@@ -46,83 +46,8 @@ namespace EveryChart
             SolidBrush TextBrush = new SolidBrush(Color.Teal);
             // (만 권)의 위치
             PointF KwanTextPoint = new PointF(GridWidth * 1.1f, GridHeight);
-            // 세로축에 있는 60
-            string SixtyText = "60";
-            // 세로축에 있는 60의 위치
-            PointF SixtyTextPoint = new PointF(GridWidth * 2.2f, GridHeight * 1.7f);
-            // 세로축에 있는 50
-            string FiftyText = "50";
-            // 세로축에 있는 50의 위치
-            PointF FiftyTextPoint = new PointF(GridWidth * 2.2f, GridHeight * 2.7f);
-            // 세로축에 있는 40
-            string FourtyText = "40";
-            // 세로축에 있는 40의 위치
-            PointF FourtyTextPoint = new PointF(GridWidth * 2.2f, GridHeight * 3.7f);
-            // 세로축에 있는 30
-            string ThirtyText = "30";
-            // 세로축에 있는 30의 위치
-            PointF ThirtyTextPoint = new PointF(GridWidth * 2.2f, GridHeight * 4.7f);
-            // 세로축에 있는 20
-            string TwentyText = "20";
-            // 세로축에 있는 20의 위치
-            PointF TwentyTextPoint = new PointF(GridWidth * 2.2f, GridHeight * 5.7f);
-            // 세로축에 있는 10
-            string VerticalTenText = "10";
-            // 세로축에 있는 10의 위치
-            PointF VerticalTenTextPoint = new PointF(GridWidth * 2.2f, GridHeight * 6.7f);
-            // 세로축에 있는 0
-            string ZeroText = "0";
-            // 세로축에 있는 0의 위치
-            PointF ZeroTextPoint = new PointF(GridWidth * 2.6f, GridHeight * 7.5f);
-            // X축 글자열
-            // 가로축에 있는 1
-            string OneText = "1";
-            // 가로축에 있는 1의 위치
-            PointF OneTextPoint = new PointF(GridWidth * 4, GridHeight * 8);
-            // 가로축에 있는 2
-            string TwoText = "2";
-            // 가로축에 있는 2의 위치
-            PointF TwoTextPoint = new PointF(GridWidth * 5, GridHeight * 8);
-            // 가로축에 있는 3
-            string ThreeText = "3";
-            // 가로축에 있는 3의 위치
-            PointF ThreeTextPoint = new PointF(GridWidth * 6, GridHeight * 8);
-            // 가로축에 있는 4
-            string FourText = "4";
-            // 가로축에 있는 4의 위치
-            PointF FourTextPoint = new PointF(GridWidth * 7, GridHeight * 8);
-            // 가로축에 있는 5
-            string FiveText = "5";
-            // 가로축에 있는 5의 위치
-            PointF FiveTextPoint = new PointF(GridWidth * 8, GridHeight * 8);
-            // 가로축에 있는 6
-            string SixText = "6";
-            // 가로축에 있는 6의 위치
-            PointF SixTextPoint = new PointF(GridWidth * 9, GridHeight * 8);
-            // 가로축에 있는 7
-            string SevenText = "7";
-            // 가로축에 있는 7의 위치
-            PointF SevenTextPoint = new PointF(GridWidth * 10, GridHeight * 8);
-            // 가로축에 있는 8
-            string EightText = "8";
-            // 가로축에 있는 8의 위치
-            PointF EightTextPoint = new PointF(GridWidth * 11, GridHeight * 8);
-            // 가로축에 있는 9
-            string NineText = "9";
-            // 가로축에 있는 9의 위치
-            PointF NineTextPoint = new PointF(GridWidth * 12, GridHeight * 8);
-            // 가로축에 있는 10
-            string HorizontalTenText = "10";
-            // 가로축에 있는 10의 위치
-            PointF HorizontalTenTextPoint = new PointF(GridWidth * 13, GridHeight * 8);
-            // 가로축에 있는 11
-            string ElevenText = "11";
-            // 가로축에 있는 11의 위치
-            PointF ElevenTextPoint = new PointF(GridWidth * 14, GridHeight * 8);
-            // 가로축에 있는 12
-            string TwelveText = "12";
             // 가로축에 있는 12의 위치
-            PointF TwelveTextPoint = new PointF(GridWidth * 15, GridHeight * 8);
+            PointF TwelveTextPoint = new PointF(GridWidth * 14.6f, GridHeight * 8);
             // 축의 색상
             Pen LinePen = Pens.Blue;
             // 아래쪽 가로축의 시작 위치
@@ -165,112 +90,83 @@ namespace EveryChart
             PointF HorizontalLineStartPoint = new PointF(GridWidth * 3, GridHeight * 2);
             // 차트의 가로 줄의 끝 위치
             PointF HorizontalLineEndPoint = new PointF(GridWidth * 16, GridHeight * 2);
+            // 차트의 숫자
+            int Number = 1;
+            // 글자화된 차트의 숫자
+            string NumberText;
+            // 차트의 숫자의 위치
+            PointF TextPoint = new PointF(GridWidth * 3.75f, GridHeight * 8);
 
+            // 차트의 선을 제외한 나머지 부분을 그린다.
             // 제목을 그린다.
             g.DrawString(Title, TitleFont, TitleBrush, TitlePoint);
-
             // 세로 축에 (만 권)을 그린다.
             g.DrawString(KwanText, TitleFont, TextBrush, KwanTextPoint);
-
-            // 세로 축에 60을 그린다.
-            g.DrawString(SixtyText, TitleFont, TextBrush, SixtyTextPoint);
-
-            // 세로 축에 50을 그린다.
-            g.DrawString(FiftyText, TitleFont, TextBrush, FiftyTextPoint);
-
-            // 세로 축에 40을 그린다.
-            g.DrawString(FourtyText, TitleFont, TextBrush, FourtyTextPoint);
-
-            // 세로 축에 30을 그린다.
-            g.DrawString(ThirtyText, TitleFont, TextBrush, ThirtyTextPoint);
-
-            // 세로 축에 20을 그린다.
-            g.DrawString(TwentyText, TitleFont, TextBrush, TwentyTextPoint);
-
-            // 세로 축에 10을 그린다.
-            g.DrawString(VerticalTenText, TitleFont, TextBrush, VerticalTenTextPoint);
-
-            // 세로 축에 0을 그린다.
-            g.DrawString(ZeroText, TitleFont, TextBrush, ZeroTextPoint);
-
-            // 가로 축에 1을 그린다.
-            g.DrawString(OneText, TitleFont, TextBrush, OneTextPoint);
-
-            // 가로 축에 2를 그린다.
-            g.DrawString(TwoText, TitleFont, TextBrush, TwoTextPoint);
-
-            // 가로 축에 3을 그린다.
-            g.DrawString(ThreeText, TitleFont, TextBrush, ThreeTextPoint);
-
-            // 가로 축에 4를 그린다.
-            g.DrawString(FourText, TitleFont, TextBrush, FourTextPoint);
-
-            // 가로 축에 5를 그린다.
-            g.DrawString(FiveText, TitleFont, TextBrush, FiveTextPoint);
-
-            // 가로 축에 6을 그린다.
-            g.DrawString(SixText, TitleFont, TextBrush, SixTextPoint);
-
-            // 가로 축에 7을 그린다.
-            g.DrawString(SevenText, TitleFont, TextBrush, SevenTextPoint);
-
-            // 가로 축에 8을 그린다.
-            g.DrawString(EightText, TitleFont, TextBrush, EightTextPoint);
-
-            // 가로 축에 9를 그린다.
-            g.DrawString(NineText, TitleFont, TextBrush, NineTextPoint);
-
-            // 가로 축에 10을 그린다.
-            g.DrawString(HorizontalTenText, TitleFont, TextBrush, HorizontalTenTextPoint);
-
-            // 가로 축에 11을 그린다.
-            g.DrawString(ElevenText, TitleFont, TextBrush, ElevenTextPoint);
-
-            // 가로 축에 12를 그린다.
-            g.DrawString(TwelveText, TitleFont, TextBrush, TwelveTextPoint);
-
             // 아래쪽 가로 축을 그린다.
             g.DrawLine(LinePen, UnderHorizonStartPoint, UnderHorizonEndPoint);
-
             // 왼쪽 세로 축을 그린다.
             g.DrawLine(LinePen, LeftVerticalStartPoint, LeftVerticalEndPoint);
-
             // 오른쪽 가로 축을 그린다.
             g.DrawLine(LinePen, UpperHorizonStartPoint, UpperHorizonEndPoint);
-
             // 오른쪽 세로 축을 그린다.
             g.DrawLine(LinePen, RightVerticalStartPoint, RightVerticalEndPoint);
-
             // 왼쪽 아래 대각선을 그린다.
             g.DrawLine(LinePen, LeftDiagonalStartPoint, LeftDiagonalEndPoint);
-
             // "판매량"을 그린다.
             g.DrawString(SalesVolumeText, TitleFont, TextBrush, SalesVolumeTextPoint);
-
             // "월"을 그린다.
             g.DrawString(MonthText, TitleFont, TextBrush, MonthTextPoint);
-
             // 차트의 세로 줄을 그린다.
             for (int i = 0; i < VerticalLineCount; i++)
             {
-                
+                NumberText = Number.ToString();
+                // 실선을 그린다.
                 g.DrawLine(LinePen, VerticalLineStartPoint, VerticalLineEndPoint);
+                // 글씨를 그린다.
+                g.DrawString(NumberText, TitleFont, TextBrush, TextPoint);
+                // 점선을 그린다.
 
                 // 세로 줄을 한 칸씩 이동시킨다.
                 VerticalLineStartPoint.X += GridWidth;
                 VerticalLineEndPoint.X += GridWidth;
+                TextPoint.X += GridWidth;
+                Number++;
             }
+
+            TextPoint = new PointF(GridWidth * 2.2f, GridHeight * 1.7f);
+            Number = 60;
 
             // 차트의 가로 줄을 그린다.
             for (int i = 0; i < HorizontalLineCount; i++)
             {
-
+                //차트에 넣을 숫자를 구한다.
+                NumberText = Number.ToString();
+                
+                // 실선을 그린다.
                 g.DrawLine(LinePen, HorizontalLineStartPoint, HorizontalLineEndPoint);
+                // 글씨를 그린다.
+                g.DrawString(NumberText, TitleFont, TextBrush, TextPoint);
+                // 차트의 가로 눈금을 그린다.
 
                 // 가로 줄을 한 칸씩 이동시킨다.
                 HorizontalLineStartPoint.Y += GridHeight;
                 HorizontalLineEndPoint.Y += GridHeight;
+
+                // 글씨를 한 칸씩 이동시킨다.
+                if (i < HorizontalLineCount - 2)
+                {
+                    TextPoint.Y += GridHeight;
+                }
+                else
+                {
+                    TextPoint.Y += GridHeight / 2.0f;
+                }
+                
+                Number -= 10;
             }
+
+            
+
         }
     }
 }
