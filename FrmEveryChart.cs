0a843e610611dc8cc4f31d7752407085d5ce8f16
totalintelli <blueintelli@gmail.com>
2016-06-03 12:15:56 +0900

    차트의 글자 및 선의 위치를 수정.

--- FrmEveryChart.cs
+++ FrmEveryChart.cs
@@ -62,18 +62,18 @@ namespace EveryChart
             // 제목 글자열
             string Title = "월별 책의 판매량";
             // 제목의 폰트
-            Font TitleFont = new Font("SansSerif", 18);
+            Font TitleFont = new Font("SansSerif", (GridWidth + GridHeight) / 8.0f);
             // 제목의 색상
             SolidBrush TitleBrush = new SolidBrush(Color.Blue);
             // 제목의 위치
-            PointF TitlePoint = new PointF(Width / 2.0f, GridHeight); // 1/ 2.0f는 제목의 위치를 정하기 위한 값으로 고정값.
+            PointF TitlePoint = new PointF(Width / 2.0f, ButtonDockPanel.Height); // 1/ 2.0f는 제목의 위치를 정하기 위한 값으로 고정값.
             // Y축 글자열
             // (만 권)
             string KwanText = "(만 권)";
             // (만 권)의 색상
             SolidBrush TextBrush = new SolidBrush(Color.Teal);
             // (만 권)의 위치
-            PointF KwanTextPoint = new PointF(GridWidth * 1.3f, GridHeight * 1.7f);
+            PointF KwanTextPoint = new PointF(GridWidth * 1.3f, ButtonDockPanel.Height + GridHeight * 0.5f);
             // 축의 색상
             Pen LinePen = Pens.Blue;
             // 아래쪽 가로축의 시작 위치
@@ -81,7 +81,7 @@ namespace EveryChart
             // 아래쪽 가로축의 끝 위치
             PointF UnderHorizonEndPoint = new PointF(GridWidth * 16.0f, GridHeight * 8.7f);
             // 왼쪽 세로축의 시작 위치
-            PointF LeftVerticalStartPoint = new PointF(GridWidth * 3.0f, GridHeight * 1.7f);
+            PointF LeftVerticalStartPoint = new PointF(GridWidth * 3.0f, ButtonDockPanel.Height + GridHeight * 0.5f);
             // 왼쪽 세로축의 끝 위치
             PointF LeftVerticalEndPoint = new PointF(GridWidth * 3.0f, panel1.Height);
             // 오른쪽 세로축의 시작 위치
@@ -103,15 +103,15 @@ namespace EveryChart
             // 차트의 세로 줄의 개수
             int VerticalLineCount = 13;
             // 차트의 세로 줄의 시작 위치
-            PointF VerticalLineStartPoint = new PointF(GridWidth * 4.0f, GridHeight * 1.7f);
+            PointF VerticalLineStartPoint = new PointF(GridWidth * 4.0f, ButtonDockPanel.Height + GridHeight * 0.5f);
             // 차트의 세로 줄의 끝 위치
-            PointF VerticalLineEndPoint = new PointF(GridWidth * 4.0f, GridHeight * 8.7f);
+            PointF VerticalLineEndPoint = new PointF(GridWidth * 4.0f, ButtonDockPanel.Height + GridHeight * 7.3f);
             // 차트의 가로 줄의 개수
             int HorizontalLineCount = 7;
             // 차트의 가로 줄의 시작 위치
-            PointF HorizontalLineStartPoint = new PointF(GridWidth * 3.0f, GridHeight * 1.7f);
+            PointF HorizontalLineStartPoint = new PointF(GridWidth * 3.0f, ButtonDockPanel.Height + GridHeight * 0.5f);
             // 차트의 가로 줄의 끝 위치
-            PointF HorizontalLineEndPoint = new PointF(GridWidth * 16.0f, GridHeight * 1.7f);
+            PointF HorizontalLineEndPoint = new PointF(GridWidth * 16.0f, ButtonDockPanel.Height + GridHeight * 0.5f);
             // 차트의 숫자
             int Number = 1;
             // 글자화된 차트의 숫자
@@ -169,7 +169,7 @@ namespace EveryChart
 
             }
 
-            TextPoint = new PointF(GridWidth * 2.2f, GridHeight * 2.4f);
+            TextPoint = new PointF(GridWidth * 2.2f, ButtonDockPanel.Height + GridHeight * 1.3f);
             Number = 60;
 
             // 차트의 가로 줄을 그린다.
