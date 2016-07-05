cd52505241f526a59684fa28d591224976c67f31
totalintelli <blueintelli@gmail.com>
2016-07-05 20:39:36 +0900

    원점의 위치를 나타내는 콤보 박스의 항목을 1개로 줄임.

--- FrmEveryChart.Designer.cs
+++ FrmEveryChart.Designer.cs
@@ -183,10 +183,7 @@
             // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "LowerLeft",
-            "LowerRight",
-            "UpperLeft",
-            "UpperRight"});
+            "LowerLeft"});
             this.comboBox1.Location = new System.Drawing.Point(325, 14);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 20);
