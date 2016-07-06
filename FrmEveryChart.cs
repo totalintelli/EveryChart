82008c0fd914c4f9a52f2366c74f090654329442
totalintelli <blueintelli@gmail.com>
2016-07-06 12:21:49 +0900

    막대 그래프의 제목을 수정함.

--- FrmEveryChart.cs
+++ FrmEveryChart.cs
@@ -754,7 +754,7 @@ namespace EveryChart
             // 선의 끝점
             PointF DataLineEndPoint;
             // 제목 글자
-            String TitleText = "월별 책의 판매량";
+            String TitleText = "연도별 매출 성장 변화 추이";
             // 제목의 색상
             SolidBrush TitleBrush = new SolidBrush(Color.Blue);
             // 제목의 위치
@@ -767,10 +767,91 @@ namespace EveryChart
             e.Graphics.DrawRectangle(LinePen, GraphRect.Left, GraphRect.Top, GraphRect.Width, GraphRect.Height);
 
             // 제목의 위치를 정한다.
-            TitlePoint = new PointF(NewGraph.RealRect.Width * 0.5f, NewGraph.GraphMargin.Top * 0.6f);
+            TitlePoint = new PointF(NewGraph.RealRect.Width * 0.3f, NewGraph.GraphMargin.Top * 0.6f);
             // 그래프의 제목을 그린다.
             e.Graphics.DrawString(TitleText, TextFont, TitleBrush, TitlePoint);
-        }
 
+            switch (NewGraph.CurrentOriginPoint)
+            {
+                case Graph.OriginPointPosition.LowerLeft:
+                    {
+                        // 그래프 가로 눈금을 그리는 부분
+                        HorizontalGridStartPoint = new PointF(GraphRect.Left, GraphRect.Top + GraphRect.Height / VerticalGridCount);
+                        HorizontalGridEndPoint = new PointF(GraphRect.Left + GraphRect.Width, GraphRect.Top + GraphRect.Height / VerticalGridCount);
+                        NumberPoint = new PointF(GraphRect.Left - 50.0f, GraphRect.Top + GraphRect.Height * 3.0f / VerticalGridCount); // 50.0f은 숫자의 X좌표 값을 설정하기 위한 값이고 3.0f는 숫자의 높이를 정하기 위한 값으로 고정값.
+                        for (int i = 0; i < BigVerticalGridCount; i++)
+                        {
+                            for (int j = 0; j < 5; j++)
+                            {
+                                LinePen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                                if (j == 4)
+                                {
+                                    LinePen.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+                                }
+                                // 가로 눈금를 그린다.
+                                e.Graphics.DrawLine(LinePen, HorizontalGridStartPoint, HorizontalGridEndPoint);
+                                HorizontalGridStartPoint.Y += GraphRect.Height / VerticalGridCount;
+                                HorizontalGridEndPoint.Y += GraphRect.Height / VerticalGridCount;
+                            }
+                            if (i > 0)
+                            {
+                                // 가로 눈금의 숫자를 그린다.
+                                e.Graphics.DrawString(((BigVerticalGridCount - i) * 10.0f).ToString(), TextFont, TextBrush, NumberPoint);
+                                NumberPoint.Y += GraphRect.Height * 5.0f / VerticalGridCount;
+                            }
+                        }
+
+                        // 그래프의 세로 눈금을 그리는 부분
+                        VerticalGridStartPoint = new PointF(GraphRect.Left + GraphRect.Width / HorizontalGridCount, GraphRect.Top);
+                        VerticalGridEndPoint = new PointF(GraphRect.Left + GraphRect.Width / HorizontalGridCount, GraphRect.Top + GraphRect.Height);
+                        NumberPoint = new PointF(VerticalGridStartPoint.X - 15.0f, VerticalGridEndPoint.Y + GraphRect.Height / VerticalGridCount);                       // 15.0f는 숫자의 X좌표를 정하기 위한 값으로 고정 값.,
+                        for (int i = 0; i < HorizontalGridCount; i++)
+                        {
+                            e.Graphics.DrawLine(LinePen, VerticalGridStartPoint, VerticalGridEndPoint);
+                            if (i < HorizontalGridCount - 1)
+                            {
+                                e.Graphics.DrawString((i + 1).ToString(), TextFont, TextBrush, NumberPoint);
+                                NumberPoint.X += GraphRect.Width / HorizontalGridCount;
+                            }
+
+                            VerticalGridStartPoint.X += GraphRect.Width / HorizontalGridCount;
+                            VerticalGridEndPoint.X += GraphRect.Width / HorizontalGridCount;
+
+                        }
+
+                        // 그래프의 점을 그리는 부분
+                        // 점을 그리는 사각형을 정의한다.
+                        //for (int i = 0; i < Data.GetLength(0); i++)
+                        //{
+                        //    DataPointRect = new RectangleF(NewGraph.GetMathXPoint(Data[i, 0], XMin, XMax + OneGridXValue).X - PointRadius,
+                        //                                            NewGraph.GetMathYPoint(Data[i, 1], YMin, YMax + OneGridYValue).Y - PointRadius, PointSize, PointSize);
+                        //    e.Graphics.FillEllipse(PointBrush, DataPointRect);
+                        //}
+
+                        // 그래프의 선을 그리는 부분
+                        //for (int i = 0; i < Data.GetLength(0); i++)
+                        //{
+                        //    DataLineStartPoint = new PointF(NewGraph.GetMathXPoint(Data[i, 0], XMin, XMax + OneGridXValue).X,
+                        //                                        NewGraph.GetMathYPoint(Data[i, 1], YMin, YMax + OneGridYValue).Y);
+                        //    if (i < Data.GetLength(0) - 1)
+                        //    {
+                        //        DataLineEndPoint = new PointF(NewGraph.GetMathXPoint(Data[i + 1, 0], XMin, XMax + OneGridXValue).X,
+                        //                                        NewGraph.GetMathYPoint(Data[i + 1, 1], YMin, YMax + OneGridYValue).Y);
+                        //        e.Graphics.DrawLine(DataLinePen, DataLineStartPoint, DataLineEndPoint);
+                        //    }
+
+                        //}
+                    }
+                    break;
+                case Graph.OriginPointPosition.LowerRight:
+                    break;
+                case Graph.OriginPointPosition.UpperRight:
+                    break;
+                case Graph.OriginPointPosition.UpperLeft:
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
